88e00a2d30684f191322e8fe066ac0718c496c6d
wjc <139726196+wjc2821296948@users.noreply.github.com>
2026-05-20 14:14:19 +0800

    Remove blocked regions and IP addresses
    Co-authored-by: cgsdn <chaogeshuodiannao@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ const upstream_path = '/'
 const upstream_mobile = 'www.google.com'
 
 // Countries and regions where you wish to suspend your service.
-const blocked_region = ['CN', 'KP', 'SY', 'PK', 'CU']
+const blocked_region = ['KP', 'SY', 'PK', 'CU']
 
 // IP addresses which you wish to block from using your service.
-const blocked_ip_address = ['0.0.0.0', '127.0.0.1']
+const blocked_ip_address = ['127.0.0.1']
 
 // Whether to use HTTPS protocol for upstream address.
 const https = true
